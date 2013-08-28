76d354ab24e798001116513728fe9cd1b8db18c4
Thomas Levine <occurrence@thomaslevine.com>
2013-08-28 16:46:40 -0700

    typo

--- src/db.py
+++ src/db.py
@@ -7,5 +7,5 @@ dt = DumpTruck(dbname = '/tmp/catalog.db')
 for data_json in os.listdir('catalogs'):
     data = json.load(open(os.path.join('catalogs', data_json)))[1:]
     dt.create_table(data[0], 'catalog', if_not_exists = True)
-    dt.create_index(['identifier'], unique = True, if_not_exists = True)
+    dt.create_index(['identifier'], 'catalog', unique = True, if_not_exists = True)
     dt.upsert(data, 'catalog')
