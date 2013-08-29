79c0ae76ee218117be406ea77df42961c3dee95e
Thomas Levine <occurrence@thomaslevine.com>
2013-08-28 17:54:46 -0700

    aoeu

--- src/format-over-time.r
+++ src/format-over-time.r
@@ -9,12 +9,17 @@ datasets$format <- sub('^x-',  '', datasets$format)
 datasets$format <- factor(datasets$format)
 
 datasets$created <- as.Date(datasets$created)
-
-# Proportion CSV over time
 datasets$csv <- datasets$format == 'csv'
-csv <- ddply(datasets, c('portal', 'csv'), function(df) {
-  df$count <- 1:nrow(df)
-  df
-})
 
-p <- ggplot(csv) + aes(x = created, y = count, group = portal, color = csv) + geom_line()
+# Proportion CSV by month
+datasets$month <- factor(
+  strftime(datasets$created, format = '%Y-%m'),
+  levels = paste(2009:2013, rep(c('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'), 5), sep = '-')
+)
+csv.monthly <- ddply(datasets, c('portal', 'month'), function(df) {
+  c(
+    prop.csv = mean(df$csv),
+    count = nrow(df)
+  )
+})
+p.monthly <- ggplot(monthly) + aes(x = month, y = prop.csv, group = portal, size = count) + geom_point()
