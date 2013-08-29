dc1387213ede80014c7ba70026389cc7866520d9
Thomas Levine <occurrence@thomaslevine.com>
2013-08-28 17:42:20 -0700

    proportion csv

---
+++ src/format-over-time.r
@@ -0,0 +1,20 @@
+library(sqldf)
+library(ggplot2)
+library(plyr)
+
+datasets <- sqldf("SELECT portal, format, created FROM catalog ORDER BY created;", dbname = '/tmp/catalog.db')
+datasets$format <- sub(';.*$', '', datasets$format)
+datasets$format <- sub('^.*/', '', datasets$format)
+datasets$format <- sub('^x-',  '', datasets$format)
+datasets$format <- factor(datasets$format)
+
+datasets$created <- as.Date(datasets$created)
+
+# Proportion CSV over time
+datasets$csv <- datasets$format == 'csv'
+csv <- ddply(datasets, c('portal', 'csv'), function(df) {
+  df$count <- 1:nrow(df)
+  df
+})
+
+# p <- ggplot(csv) + aes(x = created, y = count, group = portal) + geom_line()
