b751b003d50f3dfbec9f7fe4ad2c9109ed6ca8c1
Thomas Levine <occurrence@thomaslevine.com>
2013-08-29 18:10:15 -0700

    shapes

--- src/format-over-time.r
+++ src/format-over-time.r
@@ -12,17 +12,17 @@ datasets$created <- as.Date(datasets$created)
 datasets$csv <- datasets$format == 'csv'
 
 # Proportion CSV by month
-datasets$month <- factor(
-  strftime(datasets$created, format = '%Y-%m'),
-  levels = paste(2009:2013, rep(c('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'), 5), sep = '-')
-)
+datasets$month <- as.Date(paste0(strftime(datasets$created, format = '%Y-%m'), '-01'))
 csv.monthly <- ddply(datasets, c('portal', 'month'), function(df) {
   c(
     prop.csv = mean(df$csv),
     count = nrow(df)
   )
 })
-p.monthly <- ggplot(csv.monthly) + aes(x = month, y = prop.csv, group = portal, size = count) + geom_point()
+p.monthly <- ggplot(csv.monthly) +
+  aes(x = month, y = prop.csv, group = portal, size = count) + geom_point() +
+  scale_x_date('Month') + scale_y_continuous('Proportion CSV') +
+  scale_size_continuous('Number of datasets')
 
 # Cumulative CSV and non-csv
 csv.cum <- ddply(datasets, c('portal'), function(df) {
