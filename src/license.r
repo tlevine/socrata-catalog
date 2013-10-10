20c97183addd0449b684fe402aa3e04fd365f828
Thomas Levine <occurrence@thomaslevine.com>
2013-10-10 14:44:06 +0000

    traffic

--- src/license.r
+++ src/license.r
@@ -13,5 +13,11 @@ catalog$missouri <- catalog$portal == 'data.mo.gov'
 # PDFs
 catalog$pdf <- grepl('pdf', catalog$format, ignore.case = TRUE)
 
+# The traffic data really are tables
+# https://data.mo.gov/Traffic/2012-Traffic-Data-St-John-Ave-and-Topping-Av-PED-X/f8qf-7is2?
+# https://data.mo.gov/api/file_data/RUHsZRBZ1NEZZzmMODqg7pXeg911Vsj3ekdk0FovPhk?filename=STJOHNTOPPING_PED-X_12HR_2012B_TMC.pdf
+catalog$traffic.data <- grepl('traffic data', catalog$title, ignore.case = TRUE)
+
 # Datasets that should have licenses
-interesting <- subset(catalog, !public.domain & pdf & missouri)
+interesting <- subset(catalog, !public.domain & pdf & missouri & !traffic.data)
+row.names(interesting) <- interesting$identifier
