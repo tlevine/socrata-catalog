dadfe33228516102c68ca466a4c55134a941452d
Thomas Levine <occurrence@thomaslevine.com>
2013-10-10 14:45:52 +0000

    acs

--- src/license.r
+++ src/license.r
@@ -18,6 +18,11 @@ catalog$pdf <- grepl('pdf', catalog$format, ignore.case = TRUE)
 # https://data.mo.gov/api/file_data/RUHsZRBZ1NEZZzmMODqg7pXeg911Vsj3ekdk0FovPhk?filename=STJOHNTOPPING_PED-X_12HR_2012B_TMC.pdf
 catalog$traffic.data <- grepl('traffic data', catalog$title, ignore.case = TRUE)
 
+# ACS data. Lots of facts, but maybe the report can be copyrighted
+# https://data.mo.gov/Census/1940-2010-Census-ACS-Council-District-1-Data/wdnq-qmt4?
+# https://data.mo.gov/api/file_data/Qrn-OBDtt9fjLzeryhE8uytBNSB7ZndUOisWUwKeIRU?filename=0-0-1980To2010Census_And_2007-2011ACS_Profile_District1.pdf
+catalog$acs <- grepl('ACS', catalog$title)
+
 # Datasets that should have licenses
 interesting <- subset(catalog, !public.domain & pdf & missouri & !traffic.data)
 row.names(interesting) <- interesting$identifier
