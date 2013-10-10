b125d9bdd157ca4cabfd39de810f65db1b1954ed
Thomas Levine <occurrence@thomaslevine.com>
2013-10-10 14:37:04 +0000

    find datasets that should have licenses

---
+++ src/license.r
@@ -0,0 +1,17 @@
+library(sqldf)
+
+if (!('catalog' %in% ls())) {
+  catalog <- sqldf('select * from catalog', dbname = '/tmp/catalog.db')
+}
+
+# Public domain
+catalog$public.domain <- grepl('Public Domain', catalog$license, ignore.case = TRUE)
+
+# Missouri, because it has a lot of PDFs
+catalog$missouri <- catalog$portal == 'data.mo.gov'
+
+# PDFs
+catalog$pdf <- grepl('pdf', catalog$format, ignore.case = TRUE)
+
+# Datasets that should have licenses
+interesting <- subset(catalog, !public.domain & pdf & missouri)
